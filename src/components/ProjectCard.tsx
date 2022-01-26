c056237599911aa07c8ef0c999a85fc2aad7f7b3
gabriel <gabrielalmeidads@gmail.com>
2022-01-25 22:17:11 -0300

    Adicionando liink para página do projeto

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,11 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from '../styles/components/ProjectCard.module.css';
 
 import { faTrophy } from '@fortawesome/free-solid-svg-icons';
+import { link } from 'fs';
 
 interface ProjectCardProps {
     title: string,
     description: string,
     imgSrc: string;
+    link: string;
 }
 
 export function ProjectCard(props: ProjectCardProps) {
@@ -19,6 +21,7 @@ export function ProjectCard(props: ProjectCardProps) {
                     {props.title}
                 </h2>
                 <div className={styles.imgProject}>
+                    {/* <FontAwesomeIcon className={styles.img} icon={faTrophy} /> */}
                     <img className={styles.img} src={props.imgSrc} alt={props.title} />
                 </div>
                 <div className={styles.textProject}>
@@ -26,6 +29,9 @@ export function ProjectCard(props: ProjectCardProps) {
                         {props.description}
                     </h2>
                 </div>
+            <div>
+                <a href={props.link} >Acessar</a>
+            </div>
             </div>
         </div>
         

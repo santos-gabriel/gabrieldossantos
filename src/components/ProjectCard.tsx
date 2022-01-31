11a5ec03c77aea42bfbaf24359faf872d1d36f19
Gabriel <gabrielalmeidads@gmail.com>
2022-01-31 13:54:58 -0300

    Atualizando estilo de do card de projetos

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,7 +2,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from '../styles/components/ProjectCard.module.css';
 
-import { faTrophy } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { link } from 'fs';
 
 interface ProjectCardProps {
@@ -14,29 +14,44 @@ interface ProjectCardProps {
 
 export function ProjectCard(props: ProjectCardProps) {
     return (
-        
-        <div className={styles.cardProject}>
-            <div>
-                <h2 className={styles.titleCardProject}>{props.title}</h2>
-            </div>
-
-            <div className={styles.circleProject}>               
-                <img className={styles.img} src={props.imgSrc} alt={props.title} />
-                {/* <div className={styles.imgProject}>                    
+        <>
+            <div className={styles.cardProject}>
+                {/* <div>
+                    <h2 className={styles.titleCardProject}>{props.title}</h2>
                 </div> */}
+
+                <div className={styles.circleProject}>               
+                    <img className={styles.img} src={props.imgSrc} alt={props.title} />
+                    {/* <div className={styles.imgProject}>                    
+                    </div> */}
+                </div>
+
+                {/* <div className={styles.textProject}>
+                    <h2 className={styles.descriptionProject}>
+                        {props.description}
+                    </h2>
+                </div>
+
+                <div>
+                    <a href={props.link} >Acessar</a>
+                </div> */}
+
+                <a href={props.link} className={styles.captionProject}>
+                    <div>
+                        <p>
+                            {props.title}
+                        </p>
+                        <p>
+                            {props.description}
+                        </p>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faArrowRight}/>
+                    </div>
+                </a>
+
             </div>
 
-            <div className={styles.textProject}>
-                <h2 className={styles.descriptionProject}>
-                    {props.description}
-                </h2>
-            </div>
-
-            <div>
-                <a href={props.link} >Acessar</a>
-            </div>
-
-        </div>
-        
+    </>
     );
 }

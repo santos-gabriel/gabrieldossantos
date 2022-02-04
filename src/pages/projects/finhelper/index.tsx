2adeac077a3a573c0e4ea9478189bbe94ddfe2cd
gabriel <gabrielalmeidads@gmail.com>
2022-02-03 21:44:07 -0300

    Atualizando links e nome do projeto FinHelper

--- src/pages/projects/finhelper/index.tsx
+++ src/pages/projects/finhelper/index.tsx
@@ -4,15 +4,15 @@ import { LinkProjects } from "../../../components/NavBarProject";
 
 export default function FinHelper() {
     let links: LinkProjects[] = [
-        {icon:faGithub, link:"https://www.github.com/Santos-Gabriel", targetType:"_blank"},
-        {icon:faGooglePlay, link:"https://www.linkedin.com/in/gabriel-almeida-dos-santos", targetType:"_blank"}];
+        {icon:faGithub, link:"https://www.github.com/Santos-Gabriel/fin-helper", targetType:"_blank"},
+        {icon:faGooglePlay, link:"https://play.google.com/store/apps/", targetType:"_blank"}];
     
     
     return (
         <>
             <div>
                 <NavBarProject 
-                    projectName={'FinHelper'} 
+                    projectName={'FinHelper - Finances Helper'} 
                     projectImage="/FinHelper_purple.svg"
                     links={links}
                 />

cbdc737c53db3365ff91595fa7c004ec8423f446
gabriel <gabrielalmeidads@gmail.com>
2022-02-07 22:19:24 -0300

    Updating home project FinHelper

--- src/pages/projects/finhelper/home.tsx
+++ src/pages/projects/finhelper/home.tsx
@@ -1,17 +1,16 @@
-import { faGithub, faGooglePlay } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faGooglePlay, faInstagram, faLinkedin, faTelegram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { useContext, useEffect } from "react";
 import NavBarProject from "../../../components/NavBarProject";
-import { LinkProjects } from "../../../components/NavBarProject";
+import { LinkIcon } from "../../../models/LinkIcons";
 import styles from '../../../styles/pages/Project.module.css';
 import { NavBarProjectsContext } from "../../../contexts/NavBarProjectsContext";
-import { Footer } from "../../../components/Footer";
+import { FooterProject } from "../../../components/FooterProject";
 
 export default function Home() {
-    let links: LinkProjects[] = [
+    let links: LinkIcon[] = [
         {icon:faGithub, link:"https://www.github.com/Santos-Gabriel/fin-helper", targetType:"_blank"},
         {icon:faGooglePlay, link:"https://play.google.com/store/apps/", targetType:"_blank"}];
-        
-    
+
     const { handleFixed } = useContext(NavBarProjectsContext);    
 
     useEffect (() => {
@@ -44,33 +43,41 @@ export default function Home() {
                     </div>
                     <div>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae obcaecati praesentium deserunt veniam 
-                            doloribus, sint at pariatur enim omnis soluta odio, fugiat quo, repellat a magni odit architecto animi! 
-                            Recusandae.
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae obcaecati praesentium deserunt veniam 
-                            doloribus, sint at pariatur enim omnis soluta odio, fugiat quo, repellat a magni odit architecto animi! 
-                            Recusandae.
+                            FinHelper é um acrônimo para Finances Helper, que traduzido diretamente significa Ajudante de Finanças. 
+                            E este é seu papel, ajudar nas finanças pessoais de todos que buscam um melhor controle de sua vida financeira.
+                        </p>
+                        <br />
+                        <p>
+                            FinHelper se trata de um aplicativo com o objetivo de auxiliar nas finanças pessoais, podendo gerenciar suas receitas
+                            e despesas pessoais, categorizando por <b>Fixas</b> e <b>Não Fixas</b> tanto as despesas quanto as receitas pode ser categorizadas.
                         </p>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae obcaecati praesentium deserunt veniam 
-                            doloribus, sint at pariatur enim omnis soluta odio, fugiat quo, repellat a magni odit architecto animi! 
-                            Recusandae.
+                            Podendo ser acessado de qualquer lugar, podendo ser utilizado até mesmo sem acesso a internet. Basta apenas 
+                            um celular e uma vontade de organização financeira pessoal.
+                        </p>
+                        <p>
+                            Para segurança e privacidade FinHelper conta com proteção de acesso, seja por senha, padrão ou até mesmo reconhecimento facial ou impressão digital.
+                        </p>
+                        <p>
+                            Ainda pensando na segurança e privacidade, o aplicativo armazena seus dados em seu próprio dispositivo para que não seja armazenado dados em servidores.
                         </p>
                     </div>
                 </div>
                 <div className={styles.photos}>
                     <div className={styles.photo}>
-                        <img src="/back-gabriel.svg" alt="FinHelper" />
-                        <img src="/front-gabriel.svg" alt="FinHelper" />
+                        <img src="/home.png" alt="FinHelper" />
+                        <img src="/first_login.png" alt="FinHelper" />
+                        <img src="/add_revenue.png" alt="FinHelper" />
                     </div>
                     <div className={styles.photo}>
-                        <img src="/FinHelper_purple.svg" alt="FinHelper" />
+                        <img src="/settings.png" alt="FinHelper" />
+                        <img src="/splash.png" alt="FinHelper" />
                     </div>
                 </div>
                 
                 
             </div>
-            <Footer />
+            <FooterProject linksIcons={[]} />
         </div>
     );
 }

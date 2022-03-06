83b67a89797f91f1d4e5a006e58be0a8c348b878
gabriel <gabrielalmeidads@gmail.com>
2022-03-06 17:14:17 -0300

    Corrigindo erro de escrita em texto

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,11 +16,11 @@ export function About () {
                     <h1 className="font-zero">Sobre Gabriel</h1>
                     <img src="/front-gabriel.svg" alt="Gabriel" />
                     <p>Me chamo Gabriel dos Santos tenho {new Date().getFullYear().toString().substr(-2)} anos, sou desenvolvedor de software e formado em Análise e Desenvolvimento de Sistemas. 
-                    Iniciei-me neste mundo da tecnologia aos meus 17 anos, tive minha primeira oportunidade na área da tecnologia aos meus 18 anos, sendo como auxiliar de TI em uma clínica psiquiátrica onde junto com a equipe de TI pestava suporte e zelava 
+                    Iniciei-me neste mundo da tecnologia aos meus 17 anos, tive minha primeira oportunidade na área da tecnologia aos meus 18 anos, sendo como auxiliar de TI em uma clínica psiquiátrica onde junto com a equipe de TI prestava suporte e zelava 
                     por toda a infraestrutura de redes da clínica, mantendo sempre seu funcionamento efetivo.
                     </p>
                     <br/>
-                    <p>Atualmente estou voltado ao desenvolvimento de soluções em softwares, colaboro com uma equipe de desenvolvimento onde junto com a mesma realizo o desenvolvimento de novas soluções e presto manutenção em seus sistemas e suas API's. 
+                    <p>Atualmente estou voltado ao desenvolvimento de soluções em softwares, colaboro com uma equipe de desenvolvimento, onde junto com a mesma realizo o desenvolvimento de novas soluções e presto manutenção em seus sistemas e suas API's. 
                     Recentemente tive a oportunidade de participar juntamente a um grupo formado por três integrantes onde realizáva-mos a documentação, criação e implantação de um sistema de gestão em uma loja varejista de calçados situada no interior do estado de Goiás. Estou sempre a procura de novos desafios, aprendendo novas habilidades e melhorando as já existentes.
                     </p>
                     <br/>
